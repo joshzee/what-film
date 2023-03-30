291fe908e27ad000300820f077e2bec3febf7cdc
Josh Zarazovski <joshuazarazovski@gmail.com>
2023-03-31 09:50:19 +1100

    Added dynamic Imdb hyperlink to posters

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,25 +1,29 @@
 import PlaceHolder from "./assets/placeholder.jpg";
 
 const MovieCard = ({ movie }) => {
+  const imdbLink = "https://imdb.com/title/" + movie.imdbID;
+
   return (
-    <div className="movie">
-      <div>
-        <p>{movie.Year}</p>
-      </div>
+    <a href={imdbLink} target="_blank" rel="noreferrer">
+      <div className="movie">
+        <div>
+          <p>{movie.Year}</p>
+        </div>
 
-      <div>
-        <img
-          src={movie.Poster !== "N/A" ? movie.Poster : PlaceHolder}
-          alt={movie.Title}
-          onClick={() => console.log(movie.Title)}
-        />
-      </div>
+        <div>
+          <img
+            src={movie.Poster !== "N/A" ? movie.Poster : PlaceHolder}
+            alt={movie.Title}
+            onClick={() => console.log(movie.Title)}
+          />
+        </div>
 
-      <div>
-        <span>{movie.Type}</span>
-        <h3>{movie.Title}</h3>
+        <div>
+          <span>{movie.Type}</span>
+          <h3>{movie.Title}</h3>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
