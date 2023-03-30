c8731b6452f2b4ff62ff0af6d682cc88379488c0
Josh Zarazovski <joshuazarazovski@gmail.com>
2023-03-31 09:57:35 +1100

    Testing anchor

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,29 +1,29 @@
 import PlaceHolder from "./assets/placeholder.jpg";
 
 const MovieCard = ({ movie }) => {
-  const imdbLink = "https://imdb.com/title/" + movie.imdbID;
+  // const imdbLink = "https://imdb.com/title/" + movie.imdbID;
 
   return (
-    <a href={imdbLink} target="_blank" rel="noreferrer">
-      <div className="movie">
-        <div>
-          <p>{movie.Year}</p>
-        </div>
-
-        <div>
-          <img
-            src={movie.Poster !== "N/A" ? movie.Poster : PlaceHolder}
-            alt={movie.Title}
-            onClick={() => console.log(movie.Title)}
-          />
-        </div>
-
-        <div>
-          <span>{movie.Type}</span>
-          <h3>{movie.Title}</h3>
-        </div>
+    // <a href={imdbLink} target="_blank" rel="noreferrer">
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
       </div>
-    </a>
+
+      <div>
+        <img
+          src={movie.Poster !== "N/A" ? movie.Poster : PlaceHolder}
+          alt={movie.Title}
+          onClick={() => console.log(movie.Title)}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+    // </a>
   );
 };
 
